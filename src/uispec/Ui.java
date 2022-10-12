85519e20bda404ea26fd068de9ad8ddaebc40f2f
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2022-10-12 12:05:22 +0530

    IdSchema&UiSpec

---
+++ src/uispec/Ui.java
@@ -0,0 +1,17 @@
+package uispec;
+
+public class Ui {
+    public IdentityUi identity;
+
+    public Ui(){
+
+    }
+
+    public IdentityUi getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(IdentityUi identity) {
+        this.identity = identity;
+    }
+}
